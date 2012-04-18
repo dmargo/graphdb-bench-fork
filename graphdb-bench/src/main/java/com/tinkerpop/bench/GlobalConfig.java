756e6ba59bd8b87c816f86fe375ef36d326828ae
Peter Macko <pmacko@eecs.harvard.edu>
2012-04-18 15:22:28 -0400

    Added GlobalConfig

---
+++ graphdb-bench/src/main/java/com/tinkerpop/bench/GlobalConfig.java
@@ -0,0 +1,10 @@
+package com.tinkerpop.bench;
+
+/**
+ * @author Peter Macko <pmacko@eecs.harvard.edu>
+ */
+public class GlobalConfig {
+
+	/// The size of the transaction buffer
+	public static int transactionBufferSize = 100 * 1000;
+}
